373b25da9fa3f2af2259d11ecaafe34b9f23d14c
j3yey <202211168@gordoncollege.edu.ph>
2024-11-26 23:33:00 +0800

    topnav commit

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "../home/home.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
   isNavVisible = true;
+  isProfileDropdownVisible = false;
 
   toggleNav() {
     this.isNavVisible = !this.isNavVisible;
+  }
+
+  toggleProfileDropdown() {
+    this.isProfileDropdownVisible = !this.isProfileDropdownVisible;
   }
 }
